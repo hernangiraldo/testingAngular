0fbbc3cb13f99dbd05160acbcca6f125a2a3d070
Hernán Giraldo <hernangiraldo@Hernans-MacBook-Pro-2.local>
2018-09-14 15:21:52 -0400

    testing a pipe

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,23 @@
+import { StrengthPipe } from "./strength.pipe";
+
+describe('StrengthPipe', () => {
+    it('should display weak if stregth is 5', () => {
+        let pipe = new StrengthPipe();
+        expect(pipe.transform(5)).toEqual(`5 (weak)`);
+    })
+
+    it('should display strong if stregth is 10', () => {
+        let pipe = new StrengthPipe();
+        expect(pipe.transform(10)).toEqual(`10 (strong)`);
+    })
+
+    it('should display unbelievable if stregth is 20', () => {
+        let pipe = new StrengthPipe();
+        expect(pipe.transform(20)).toEqual(`20 (unbelievable)`);
+    })
+
+    it('should display unbelievable if stregth is 21', () => {
+        let pipe = new StrengthPipe();
+        expect(pipe.transform(21)).toEqual(`21 (unbelievable)`);
+    })
+})
